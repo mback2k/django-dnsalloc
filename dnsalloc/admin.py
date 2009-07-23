d7bf7d7b59e86e198b9a44547d3faa7b95b5131b
Marc Hoersken <info@marc-hoersken.de>
2009-07-23 16:33:40 +0200

    Added inline editing to admin interface

--- dnsalloc/admin.py
+++ dnsalloc/admin.py
@@ -1,19 +1,23 @@
 from django.contrib import admin
 from dnsalloc.models import Service, Result
 
+class ResultInline(admin.TabularInline):
+    model = Result
+
 class ServiceAdmin(admin.ModelAdmin):
     fields = ('user', 'hostname', 'services', 'enabled')
     list_filter = ('user', 'hostname', 'enabled')
     list_display = ('user', 'hostname',  'enabled')
     list_editable = ('hostname', 'enabled')
     date_hierarchy = 'crdate'
+    inlines = [ResultInline]
 
 class ResultAdmin(admin.ModelAdmin):
     fields = ('service', 'status', 'crdate')
     list_filter = ('service', 'status')
     list_display = ('service', 'status', 'crdate')
     date_hierarchy = 'crdate'
-
+    
 try:
     admin.site.register(Service, ServiceAdmin)
     admin.site.register(Result, ResultAdmin)
