30d46ba74690c5541b63cef79d6ec654b6d1ea84
Marc Hoersken <info@marc-hoersken.de>
2009-07-25 20:35:28 +0200

    Improved performance of CipherProperty

--- dnsalloc/props.py
+++ dnsalloc/props.py
@@ -8,14 +8,8 @@ class CipherProperty(db.Property):
     
     def get_value_for_datastore(self, model_instance):
         value = super(CipherProperty, self).get_value_for_datastore(model_instance)
-        if isinstance(value, basestring) or isinstance(value, db.Text):
-            return db.Blob(XOR.new(MD5.new(self.model_class.kind()).digest()).encrypt(value))
+        if isinstance(value, basestring):
+            value = db.Blob(XOR.new(MD5.new(self.model_class.kind()).digest()).encrypt(value))
         elif isinstance(value, db.Blob):
-            return db.Blob(value)
-    
-    def make_value_from_datastore(self, value):
-        value = super(CipherProperty, self).make_value_from_datastore(value)
-        if isinstance(value, db.Blob):
-            return unicode(XOR.new(MD5.new(self.model_class.kind()).digest()).decrypt(value))
-        elif isinstance(value, basestring) or isinstance(value, db.Text):
-            return unicode(value)
+            value = db.Blob(value)
+        return value
