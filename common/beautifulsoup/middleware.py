2dd05c19a1c7325e2db11ad3b0352db4c36af934
Marc Hoersken <info@marc-hoersken.de>
2009-08-11 23:28:50 +0200

    Improved error handling in BeautifulSoup middleware

--- common/beautifulsoup/middleware.py
+++ common/beautifulsoup/middleware.py
@@ -5,21 +5,34 @@ from BeautifulSoup import BeautifulSoup
 
 class BeautifulSoupMiddleware(object):
     def process_response(self, request, response):
-        if 'Content-Type' in response and response['Content-Type'].startswith('text/html'):
-            if response['Content-Type'].find('charset=') >= 0:
-                encoding = response['Content-Type'].split('charset=')[1]
-                
-            else:
-                encoding = settings.DEFAULT_CHARSET
+        try:
+            dump = response.content
+        except TypeError:
+            return response
+        
+        if not response.content:
+            return response
+        
+        if not 'Content-Type' in response:
+            return response
+        
+        if not response['Content-Type'].startswith('text/html'):
+            return response
+        
+        if response['Content-Type'].find('charset=') >= 0:
+            encoding = response['Content-Type'].split('charset=')[1]
             
-            cache_key = 'BeautifulSoup%X' % abs(hash(response.content))
-            cache_content = cache.get(cache_key, None)
+        else:
+            encoding = settings.DEFAULT_CHARSET
+        
+        cache_key = 'BeautifulSoup%X' % abs(hash(response.content))
+        cache_content = cache.get(cache_key, None)
+        
+        if cache_content is None:
+            response.content = unicode(BeautifulSoup(response.content, fromEncoding=encoding, convertEntities=BeautifulSoup.HTML_ENTITIES))
+            cache.set(cache_key, response.content, settings.CACHE_MIDDLEWARE_SECONDS)
             
-            if cache_content is None:
-                response.content = unicode(BeautifulSoup(response.content, fromEncoding=encoding, convertEntities=BeautifulSoup.HTML_ENTITIES))
-                cache.set(cache_key, response.content, settings.CACHE_MIDDLEWARE_SECONDS)
-                
-            else:
-                response.content = cache_content
+        else:
+            response.content = cache_content
 
         return response
