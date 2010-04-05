c5b816c52ededd34512a560e71284c12f8622905
Marc Hoersken <info@marc-hoersken.de>
2010-04-05 17:07:57 +0200

    Updated jsonrpc methods to use Django instead of GAE database methods

--- dnsalloc/methods.py
+++ dnsalloc/methods.py
@@ -1,36 +1,33 @@
 import base64
 import datetime
-from google.appengine.ext import db
 from google.appengine.api import urlfetch
 from dnsalloc.models import Service, Result
 from jsonrpc.decorators import jsonrpc_function
 
 @jsonrpc_function
-def getServices(limit, bookmark=None):
-    if isinstance(bookmark, basestring):
-        bookmark = db.Key(bookmark)
+def getServices(limit=25, offset=None):
+    if offset:
+        return Service.objects.all().filter(enabled=True).filter(id__gt=offset).order_by('id')[:limit].values('id', 'hostname')
     else:
-        bookmark = db.Key.from_path('\0', '\0')
-    
-    return map(lambda x: {'key': str(x.key()), 'hostname': x.hostname}, Service.all().filter('enabled = ', True).filter('__key__ > ', bookmark).order('__key__').fetch(limit))
+        return Service.objects.all().filter(enabled=True).order_by('id')[:limit].values('id', 'hostname')
 
 @jsonrpc_function
-def setService(key, status, host):
-    service = Service.get(key)
-    if service:
-        if status != service.status and host:
-            try:
-                status = urlfetch.fetch('https://updates.dnsomatic.com/nic/update?hostname=%s&myip=%s' % (service.services, host), None, 'GET', {'Authorization': 'Basic ' + base64.b64encode('%s:%s' % (service.username, service.password))}).content
-            except:
-                return
+def setService(id, status, host):
+    try:
+        service = Service.objects.get(id=id)
+    except Service.DoesNotExist:
+        return
         
-        if status != service.status:
-            result = Result()
-            result.service_id = service.key().id()
-            result.status = status
-            result.put()
-
-        service.enabled = False if status in ['dnserr', 'nohost', 'badauth'] else service.enabled
-        service.waiting = False
-        service.update = datetime.datetime.now()
-        service.put()
+    if status != service.status and host:
+        try:
+            status = urlfetch.fetch('https://updates.dnsomatic.com/nic/update?hostname=%s&myip=%s' % (service.services, host), None, 'GET', {'Authorization': 'Basic ' + base64.b64encode('%s:%s' % (service.username, service.password))}).content
+        except:
+            return
+            
+    if status != service.status:
+        Result.objects.create(service=service, status=status)
+        
+    service.enabled = False if status in ['dnserr', 'nohost', 'badauth'] else service.enabled
+    service.waiting = False
+    service.update = datetime.datetime.now()
+    service.save()
