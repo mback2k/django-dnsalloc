f3046af32249c0a3d7a057a2a668818e0636e3df
Marc Hoersken <info@marc-hoersken.de>
2012-01-01 20:57:57 +0100

    Fixed absolute url

--- dnsalloc/tasks.py
+++ dnsalloc/tasks.py
@@ -3,12 +3,13 @@ import urllib2
 import datetime
 from celery.decorators import task
 from django.core.urlresolvers import reverse
+from django.contrib.sites.models import Site
 from dnsalloc.models import Service, Result
 
 @task()
 def task_start_worker():
     for service in Service.objects.filter(enabled=True):
-    	task_update_service.apply_async(args=[service.id])
+        task_update_service.apply_async(args=[service.id])
 
 @task()
 def task_update_service(service_id):
@@ -46,10 +47,13 @@ def task_update_service(service_id):
             service.save()
 
         if not service.enabled:
+            current_site = Site.objects.get_current()
+            edit_service = 'https://%s%s' % (current_site.domain, reverse('dnsalloc.views.edit_service', kwargs={'service_id': service.id}))
+
             service.user.email_user('DNS Allocator - Update failure for %s - Action required!' % service.hostname,
                                     'DNS Allocator tried to update your host "%s", but failed to do so, because of the following error code:\n\n' \
-                                    'https://dnsalloc.uxnr.de/%s\n\n' \
-                                    'Please go to %s and update your configuration!' % (service.hostname, status, reverse('dnsalloc.views.edit_service', kwargs={'service_id': service.id})))
+                                    '%s\n\n' \
+                                    'Please go to %s and update your configuration!' % (service.hostname, status, edit_service))
 
     except Service.DoesNotExist, e:
         pass
