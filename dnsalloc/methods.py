e8e2c2b7c76aec821563fea8279d96c0f8416227
Marc Hoersken <info@marc-hoersken.de>
2012-05-12 13:09:15 +0200

    Added ETag and Last-Modified handling

--- dnsalloc/methods.py
+++ dnsalloc/methods.py
@@ -2,8 +2,20 @@ from django.http import HttpResponse, HttpResponseForbidden, HttpResponseBadRequ
 from dnsalloc.models import Result
 from django.shortcuts import get_object_or_404
 from django.contrib.auth.decorators import login_required
+from django.views.decorators.http import condition
 from django.core import serializers
 
+def api_results_etag(request, format='json'):
+    if not format in ['xml', 'json', 'yaml']:
+        return None
+    return 'id-%d' % Result.objects.latest('crdate').id
+
+def api_results_last_modified(request, format='json'):
+    if not format in ['xml', 'json', 'yaml']:
+        return None
+    return Result.objects.latest('crdate').crdate
+
+@condition(etag_func=api_results_etag, last_modified_func=api_results_last_modified)
 def api_results(request, format='json'):
     if not format in ['xml', 'json', 'yaml']:
         return HttpResponseBadRequest()
