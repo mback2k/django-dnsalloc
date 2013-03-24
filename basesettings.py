c629c9530f177ed7ae8924e11329c5f3565de7ce
Marc Hoersken <info@marc-hoersken.de>
2013-03-24 18:26:51 +0100

    Updated logging configuration

--- basesettings.py
+++ basesettings.py
@@ -143,27 +143,15 @@ TEMPLATE_DIRS = (os.path.join(os.path.dirname(__file__), 'templates'),)
 # more details on how to customize your logging configuration.
 LOGGING = {
     'version': 1,
-    'disable_existing_loggers': False,
     'handlers': {
         'stream': {
             'level': 'WARNING',
             'class': 'logging.StreamHandler',
         },
-        'mail_admins': {
-            'level': 'ERROR',
-            'class': 'django.utils.log.AdminEmailHandler',
-        },
     },
     'loggers': {
         'django': {
             'handlers': ['stream'],
-            'level': 'DEBUG',
-            'propagate': True,
-        },
-        'django.request': {
-            'handlers': ['stream', 'mail_admins'],
-            'level': 'INFO',
-            'propagate': True,
         },
     },
 }
